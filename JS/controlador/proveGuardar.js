080cf40cb215e0d7d291a789a495eecb7ab3ac05
sesevillaga <33072539+sesevillaga@users.noreply.github.com>
2017-11-15 02:58:30 -0600

    Proveedores

--- JS/controlador/proveGuardar.js
+++ JS/controlador/proveGuardar.js
@@ -3,7 +3,7 @@ var gestor = require('./JS/gestorProve');
 function guardar(){
 alert("Hola boton");
 var nombre = document.getElementById('nombre').value;
-var edad = document.getElementById('rfc').value;
+var rfc = document.getElementById('rfc').value;
 var email = document.getElementById('email').value;
 var direccion = document.getElementById('direccion').value;
 
