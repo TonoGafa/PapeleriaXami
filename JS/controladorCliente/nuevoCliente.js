6dce8cac05b34c606e7c210811798b32cbc02ba1
TonoGafa <33079826+TonoGafa@users.noreply.github.com>
2017-11-14 23:44:52 -0600

    Clientes
    Se creo las funciones de guardar contactos

---
+++ JS/controladorCliente/nuevoCliente.js
@@ -0,0 +1,13 @@
+var gestor = require('./js/gestorCliente');
+
+function guardar(){
+
+var nombre = document.getElementById('nombre').value;
+var ape_pa = document.getElementById('Ape_Pa').value;
+var ape_ma = document.getElementById('Ape_Ma').value;
+var edad = document.getElementById('edad').value;
+var email = document.getElementById('email').value;
+
+gestor.guardarContacto(nombre,edad,email);
+
+}
