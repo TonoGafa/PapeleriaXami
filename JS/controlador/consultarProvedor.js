a7c904934a57076c6cebf25efb2b053c3811bd67
Chayras34 <spider_kio_56@hotmail.com>
2017-11-15 00:51:31 -0600

    Funcion Eliminar
    Agregar Funcion eliminar para el boton eliminar

--- JS/controlador/consultarProvedor.js
+++ JS/controlador/consultarProvedor.js
@@ -1,8 +1,16 @@
-var gestor = require('./js/gestor');
+var gestor = require('./JS/gestor');
 
 
 function consultar(){
   var contenido = document.getElementById('contenido');
 
   gestor.consultar(contenido);
+}
+
+function eliminar(id,rev){
+  var res = confirm('Esta seguro de eliminar el documento?');
+  if (res==1) {
+    gestor.eliminar(id,rev);
+  }
+
 }
